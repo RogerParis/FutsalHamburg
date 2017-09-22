efc5300359aff94bbbe5d51fd29179d104bb21fb
RogerParis <roger.parisvicent@gmail.com>
2017-09-22 09:58:12 +0200

    adds mapview

--- src/screens/contact/contactScreen/index.js
+++ src/screens/contact/contactScreen/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 import I18n from '../../../config/I18n';
 import { screenContaining } from '../../../components/containers/screenContainer';
+
+const { width } = Dimensions.get('window');
 
 class ContactScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -15,6 +18,15 @@ class ContactScreen extends React.Component {
     return (
       <View style={{ flex: 1 }}>
         <Text>{'ContactScreen'}</Text>
+        <MapView
+          style={{ flex: 1, width }}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
       </View>
     );
   }
