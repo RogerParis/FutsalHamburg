99d2f3cda6b157f56d94798856bf05f41424cee1
RogerParis <roger.parisvicent@gmail.com>
2017-10-31 13:43:47 +0100

    change translation files to json

--- src/config/I18n/index.js
+++ src/config/I18n/index.js
@@ -1,6 +1,6 @@
 import I18n from 'react-native-i18n';
-import en from './strings.en';
-import de from './strings.de';
+import en from './strings/en.json';
+import de from './strings/de.json';
 
 I18n.defaultLocale = 'en';
 I18n.fallbacks = true;
